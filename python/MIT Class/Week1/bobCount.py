d36be51c900408c181fdbb7ffd1168eee8f36e09
Jim DAgostino <jdagostino2@gmail.com>
2017-06-02 16:18:17 -0400

    EOD commit 6-2-17

--- python/MIT Class/Week1/bobCount.py
+++ python/MIT Class/Week1/bobCount.py
@@ -1,4 +1,4 @@
-s = 'oboolobxbobobolhbobob'
+s = 'bobobob'
 start = 0
 end = 3
 count = 0
@@ -6,7 +6,7 @@ count = 0
 for char in s:
     test = s[start:end]
     if test == "bob":
-        count = count+1
-    start = start+1
-    end = end + 1
+        count += 1
+    start += 1
+    end += 1
 print('Number of times bob occurs is: ' + str(count))
